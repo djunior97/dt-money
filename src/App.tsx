5a12b72242f9386cc7511dd2b4a41d8c2781f205
Dirceu Jr <dirceu.junior@ntconsultcorp.com>
2021-05-10 21:27:31 -0300

    feat: create dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Header } from "./components/Header";
-import { GlobalStyle } from "./styles/global";
+import { Dashboard } from "components/Dashboard";
 
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <>
-     <Header />
-     <GlobalStyle />
+      <Header />
+      <Dashboard />
+      <GlobalStyle />
     </>
   );
 }
-
