c709bd80445e87b164b5d60a299253c39bd8b909
Dirceu Jr <dirceu.junior@ntconsultcorp.com>
2021-05-12 10:52:09 -0300

    feat: create server with mirage js

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createServer } from "miragejs";
+import { App } from "./App";
+
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Transaction",
+          amount: 400,
+          type: "deposit",
+          createdAt: new Date(),
+          category: "Food",
+        },
+      ];
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
