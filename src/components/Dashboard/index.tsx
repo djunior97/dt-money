4a6d30b6c172ff7b1ee17099438384c2d9d7d6bf
Dirceu Jr <dirceu.junior@ntconsultcorp.com>
2021-05-12 10:52:46 -0300

    feat: add transactions table to dashboard

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import { Summary } from "components/Summary";
+import { TransactionsTable } from "components/TransactionsTable";
 
 import { Container } from "./styles";
 
@@ -6,6 +7,7 @@ export function Dashboard() {
   return (
     <Container>
       <Summary />
+      <TransactionsTable />
     </Container>
   );
 }
